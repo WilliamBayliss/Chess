cc5f10e089c1bd8ce790250c27053b5958792d8c
williambayliss <wmbayliss@gmail.com>
2021-03-19 00:03:46 -0400

    Changed square to take its name from array at initialization. Coordinate will be a two item array, first being array of x and y values and the second being a string name

--- lib/square.rb
+++ lib/square.rb
@@ -1,8 +1,8 @@
 class Square
     attr_reader :coordinate, :name, :piece
     def initialize coordinate
-        @coordinate = coordinate
-        @name = nil
+        @coordinate = coordinate[0]
+        @name = coordinate[1]
         @piece = nil
     end
 end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -3,23 +3,24 @@ require './lib/square.rb'
 describe Square do
     describe "#initialize" do
         it "initializes" do
-            square = Square.new([0,0])
+            square = Square.new([[0,0], "A1"])
             expect(square).to_not eql(nil)
         end
 
         it "initializes with the given coordinate attr" do
-            square = Square.new([0,0])
+            square = Square.new([[0,0], "A1"])
             expect(square.coordinate).to eql([0,0])
         end
 
-        it "initializes name value to nil" do
-            square = Square.new([0,0])
-            expect(square.name).to eql(nil)
+        it "initializes name value to of last item in coordinate" do
+            square = Square.new([[0,0], "A1"])
+            expect(square.name).to eql("A1")
         end
 
         it "initializes piece value to nil" do
-            square = Square.new([0,0])
+            square = Square.new([[0,0], "A1"])
             expect(square.piece).to eql(nil)
         end
     end
+
 end
