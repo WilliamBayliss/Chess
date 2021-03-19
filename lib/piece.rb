465515dddda26a3408dbdc7ee004e0062acff24c
williambayliss <wmbayliss@gmail.com>
2021-03-19 18:05:25 -0400

    Added method to check if square is diagonal from Piece @square and added unit tests

--- lib/piece.rb
+++ lib/piece.rb
@@ -7,4 +7,14 @@ class Piece
     def move square
         @square = square
     end
+
+    def diagonal? other_square
+        if other_square.nil?
+            return false
+        elsif @square.coordinate[0] != other_square.coordinate[0] && @square.coordinate[1] != other_square.coordinate[1]
+            return true
+        else 
+            return false
+        end
+    end
 end
