85d74fffa3ec8916de663cd811e47512e6f54a91
williambayliss <wmbayliss@gmail.com>
2021-03-23 23:13:18 -0400

    Added initialize that supers inherited class with logic to set symbol depending on color attr

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,4 +1,12 @@
 require './piece.rb'
 class Rook < Piece
-
+    def initialize color
+        super(color)
+        
+        if @color == "wht"
+            set_symbol("♖")
+        elsif @color == "blk"
+            set_symbol("♜")
+        end
+    end
 end
