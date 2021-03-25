42943728e45be06d94ff7e03aac1142c0d76b1b9
williambayliss <wmbayliss@gmail.com>
2021-03-25 14:27:45 -0400

    Added moves_history array to Game class to allow for each move to save the player, opponent, piece used in the move, square the piece was moved to, and the board state and for each move to be saved in an array, also added unit tests for the history feature

--- lib/game/game.rb
+++ lib/game/game.rb
@@ -1,10 +1,17 @@
 class Game
+    attr_accessor :moves_history
     def initialize
-
+        @moves_history = []
     end
 
     def create_board
         board = Board.new
         board.create_board(board.board_array)
+
+        board
+    end
+
+    def record_move move
+        @moves_history.append(move)
     end
 end
--- spec/game_spec/game_spec.rb
+++ spec/game_spec/game_spec.rb
@@ -5,5 +5,115 @@ describe Game do
             game = Game.new
             expect(game).to_not eql(nil)
         end
+
+        it "initializes moves_history to empty array" do
+            game = Game.new
+            expect(game.moves_history).to eql([])
+        end
     end
+
+    describe "#record_move" do
+        it "adds a move to the @move_history" do
+            game = Game.new
+            move = Move.new("player_one")
+            game.record_move(move)
+            expect(game.moves_history[-1]).to eql(move)
+        end
+
+        it "allows the player in a move to be accessed" do
+            game = Game.new
+            board = game.create_board
+            player_one = Player.new("Will", "wht")
+            player_two = Player.new("Fernando", "blk")
+            square = board["A5"]
+            rook = Rook.new("wht")
+            rook.move(square)
+            square.set_piece(rook)
+            move = Move.new(player_one)
+            move.get_opponent(player_two)
+            move.get_square(square)
+            move.get_piece(square)
+            move.get_board(board)
+            game.record_move(move)
+
+            expect(game.moves_history[0].player).to eql(player_one)
+        end
+
+        it "allows the opponent in a move to be accessed" do
+            game = Game.new
+            board = game.create_board
+            player_one = Player.new("Will", "wht")
+            player_two = Player.new("Fernando", "blk")
+            square = board["A5"]
+            rook = Rook.new("wht")
+            rook.move(square)
+            square.set_piece(rook)
+            move = Move.new(player_one)
+            move.get_opponent(player_two)
+            move.get_square(square)
+            move.get_piece(square)
+            move.get_board(board)
+            game.record_move(move)
+
+            expect(game.moves_history[0].opponent).to eql(player_two)
+        end
+
+        it "allows the piece moved in a move to be accessed" do
+            game = Game.new
+            board = game.create_board
+            player_one = Player.new("Will", "wht")
+            player_two = Player.new("Fernando", "blk")
+            square = board["A5"]
+            rook = Rook.new("wht")
+            rook.move(square)
+            square.set_piece(rook)
+            move = Move.new(player_one)
+            move.get_opponent(player_two)
+            move.get_square(square)
+            move.get_piece(square)
+            move.get_board(board)
+            game.record_move(move)
+
+            expect(game.moves_history[0].piece).to eql(rook)
+        end
+
+        it "allows the square a piece was moved to in a move to be accessed" do
+            game = Game.new
+            board = game.create_board
+            player_one = Player.new("Will", "wht")
+            player_two = Player.new("Fernando", "blk")
+            square = board["A5"]
+            rook = Rook.new("wht")
+            rook.move(square)
+            square.set_piece(rook)
+            move = Move.new(player_one)
+            move.get_opponent(player_two)
+            move.get_square(square)
+            move.get_piece(square)
+            move.get_board(board)
+            game.record_move(move)
+
+            expect(game.moves_history[0].square).to eql(square)
+        end
+
+        it "saves the board state at the end of the move" do
+            game = Game.new
+            board = game.create_board
+            player_one = Player.new("Will", "wht")
+            player_two = Player.new("Fernando", "blk")
+            square = board["A5"]
+            rook = Rook.new("wht")
+            rook.move(square)
+            square.set_piece(rook)
+            move = Move.new(player_one)
+            move.get_opponent(player_two)
+            move.get_square(square)
+            move.get_piece(square)
+            move.get_board(board)
+            game.record_move(move)
+
+            expect(game.moves_history[0].board).to eql(board)
+        end
+    end
+
 end
