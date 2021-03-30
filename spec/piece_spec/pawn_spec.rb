754c01bafe55acc737ac46b509952855c694e4c9
williambayliss <wmbayliss@gmail.com>
2021-03-30 12:53:43 -0400

    Refactored unit tests

--- spec/piece_spec/pawn_spec.rb
+++ spec/piece_spec/pawn_spec.rb
@@ -26,29 +26,7 @@ describe Pawn do
             pawn = Pawn.new("blk")
             expect(pawn.moved).to eql(false)
         end
-
-        it "initializes with an en passant value of nil" do
-            pawn = Pawn.new("blk")
-            expect(pawn.en_passant).to eql(nil)
-        end
     end
 
-    describe "#set_moved" do
-        it "sets the moved attr of a pawn to true" do
-            pawn = Pawn.new("blk")
-            pawn.set_moved
-            expect(pawn.moved).to eql(true)
-        end
-    end
-
-    describe "#register_en_passant" do
-        it "sets the en_passant value to a move" do
-            move = Move.new("player_one")
-            pawn = Pawn.new("blk")
-            pawn.register_en_passant(move)
-
-            expect(pawn.en_passant).to eql(move)
-        end
-    end
 
 end
