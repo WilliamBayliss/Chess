e6f46778b0e1985a1a1205d6ad880bbe2f849ae5
williambayliss <wmbayliss@gmail.com>
2021-04-09 23:01:06 -0400

    Added @king value to player that inits to nil, and added set_king method that will save the king to that value

--- lib/players/player.rb
+++ lib/players/player.rb
@@ -1,7 +1,12 @@
 class Player
-    attr_reader :name, :color
+    attr_reader :name, :color, :king
     def initialize name, color
         @name = name
         @color = color
+        @king = nil
+    end
+
+    def set_king king
+        @king = king
     end
 end 
