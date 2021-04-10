aa8b00657352230805182f438cad0475e2905e16
williambayliss <wmbayliss@gmail.com>
2021-04-09 20:54:54 -0400

    Refactored unit tests for record_move

--- spec/game_spec/game_spec.rb
+++ spec/game_spec/game_spec.rb
@@ -41,54 +41,20 @@ describe Game do
             game = Game.new
             board = game.create_board
             player_one = Player.new("Will", "wht")
-            player_two = Player.new("Fernando", "blk")
-            square = board["A5"]
-            rook = Rook.new("wht")
-            rook.set_square(square)
-            square.set_piece(rook)
             move = Move.new(player_one)
-            move.get_opponent(player_two)
-            move.get_square(square)
-            move.get_piece(square)
-            move.get_board(board)
             game.record_move(move)
 
             expect(game.moves_history[0].player).to eql(player_one)
         end
 
-        it "allows the opponent in a move to be accessed" do
-            game = Game.new
-            board = game.create_board
-            player_one = Player.new("Will", "wht")
-            player_two = Player.new("Fernando", "blk")
-            square = board["A5"]
-            rook = Rook.new("wht")
-            rook.set_square(square)
-            square.set_piece(rook)
-            move = Move.new(player_one)
-            move.get_opponent(player_two)
-            move.get_square(square)
-            move.get_piece(square)
-            move.get_board(board)
-            game.record_move(move)
-
-            expect(game.moves_history[0].opponent).to eql(player_two)
-        end
 
         it "allows the piece moved in a move to be accessed" do
             game = Game.new
             board = game.create_board
-            player_one = Player.new("Will", "wht")
-            player_two = Player.new("Fernando", "blk")
-            square = board["A5"]
+            player_one = Player.new("William", "wht")
             rook = Rook.new("wht")
-            rook.set_square(square)
-            square.set_piece(rook)
             move = Move.new(player_one)
-            move.get_opponent(player_two)
-            move.get_square(square)
-            move.get_piece(square)
-            move.get_board(board)
+            move.get_piece(rook)
             game.record_move(move)
 
             expect(game.moves_history[0].piece).to eql(rook)
@@ -96,18 +62,11 @@ describe Game do
 
         it "allows the square a piece was moved to in a move to be accessed" do
             game = Game.new
-            board = game.create_board
+            game.create_board
             player_one = Player.new("Will", "wht")
-            player_two = Player.new("Fernando", "blk")
-            square = board["A5"]
-            rook = Rook.new("wht")
-            rook.set_square(square)
-            square.set_piece(rook)
+            square = game.board["A5"]
             move = Move.new(player_one)
-            move.get_opponent(player_two)
             move.get_square(square)
-            move.get_piece(square)
-            move.get_board(board)
             game.record_move(move)
 
             expect(game.moves_history[0].square).to eql(square)
@@ -115,21 +74,14 @@ describe Game do
 
         it "saves the board state at the end of the move" do
             game = Game.new
-            board = game.create_board
+            game.create_board
             player_one = Player.new("Will", "wht")
-            player_two = Player.new("Fernando", "blk")
-            square = board["A5"]
-            rook = Rook.new("wht")
-            rook.set_square(square)
-            square.set_piece(rook)
             move = Move.new(player_one)
-            move.get_opponent(player_two)
-            move.get_square(square)
-            move.get_piece(square)
-            move.get_board(board)
+  
+            move.get_board(game.board)
             game.record_move(move)
 
-            expect(game.moves_history[0].board).to eql(board)
+            expect(game.moves_history[0].board).to eql(game.board)
         end
     end
 
